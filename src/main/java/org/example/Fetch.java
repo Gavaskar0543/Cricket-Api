28376726e619def53a70c5d6af9ccf43ed54409d
Gavaskar0543 <gavaskaraeg@gmail.com>
2024-05-27 19:50:21 +0530

    okHttp dependency added

---
+++ src/main/java/org/example/Fetch.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.io.IOException;
+import  okhttp3.OkHttpClient;
+import  okhttp3.Request;
+import okhttp3.Response;
+public class Fetch {
+    final  OkHttpClient  client = new OkHttpClient();
+    String run(String url) throws IOException{
+        Request request = new Request()
+    }
+
+}
